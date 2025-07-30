ba21263439383b673195dbbb7212bd943a565947
Enderman9424 <ruzgar.cinar0601@gmail.com>
2025-07-30 17:33:44 +0300

    Add MoreAnswerQuestion class and update scoring logic in quiz

--- logic.py
+++ logic.py
@@ -23,6 +23,22 @@ class Question:
             else:
                 buttons.append(Button(label=option, style=discord.ButtonStyle.primary, custom_id=f"wrong_{i}"))
         return buttons
+    
+class MoreAnswerQuestion(Question):
+    def __init__(self, text, answer_ids: list, *options):
+        self.__text = text
+        self.__answer_ids = set(answer_ids)
+        self.options = options
+
+    @property
+    def gen_buttons(self):
+        buttons = []
+        for i, option in enumerate(self.options):
+            if i in self.__answer_ids:
+                buttons.append(Button(label=option, style=discord.ButtonStyle.primary, custom_id=f"correct_{i}"))
+            else:
+                buttons.append(Button(label=option, style=discord.ButtonStyle.primary, custom_id=f"wrong_{i}"))
+        return buttons
 
 # Görev 4 - Listeyi sorularınızla doldurun
 quiz_questions = [
@@ -31,6 +47,7 @@ quiz_questions = [
    Question("Kediler hangi kitapları okumayı sever?", 3, "Kişisel gelişim kitapları", "Zaman yönetimi: Günde 18 saat nasıl uyunur","Sahibinizden 5 dakika erken uyumanın 101 yolu", "İnsan yönetimi rehberi"),
    Question("Python'da liste oluşturmak için hangi işaret kullanılır?", 2, "Süslü parantezler", "Köşeli parantezler", "Parantezler", "Tırnak işaretleri"),
    Question("Python'da rastgele sayı üretmek için hangi modül kullanılır?", 1,"math", "random",  "os", "rastgele"),
-   Question("Python'da bir fonksiyon nasıl tanımlanır?", 0, "def anahtar kelimesi ile", "function anahtar kelimesi ile", "func anahtar kelimesi ile", "define anahtar kelimesi ile")
+   Question("Python'da bir fonksiyon nasıl tanımlanır?", 0, "def anahtar kelimesi ile", "function anahtar kelimesi ile", "func anahtar kelimesi ile", "define anahtar kelimesi ile"),
+   MoreAnswerQuestion("Aşağıdakilerden hangisi pythonun veri tiplerinden biridir?", [0, 1, 2], "int", "str", "list", "dict", "set", "data", )
 ]
 
